da5ed732e2efc271a92723e142803902ef136d28
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-20 16:27:13 -0500

    added film list

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, request, send_file
 import random
 app = Flask(__name__)
 
+load_dotenv()
+apikey = os.getenv('API_KEY')
 
 @app.route('/')
 def home_page():
@@ -20,23 +22,24 @@ def character_page():
     Displays the the character page for SWAPI
     """
     character_id = request.args.get('character-id')
+    limit = 1
     if not character_id:
         character_id = '1'
     response = requests.get(f"https://swapi.py4e.com/api/people/{character_id}/")
     character_data = response.json()
-
     try:
         films = [requests.get(film).json()['title'] for film in character_data['films']]
     except KeyError:
         print('KeyError: Films Not Found')
         films = ['N/A']
-    
     try:
         character_name = character_data['name']
     except KeyError:
         print('KeyError: Character Not Found')
         character_name = 'sad'
-
+    response = requests.get(
+        f"https://tenor.googleapis.com/v2/search?q={character_name}&key={apikey}&limit={limit}")
+    gifs = json.loads(response.content).get('results')
     context = {
     "character": character_data,
     'films': films,
